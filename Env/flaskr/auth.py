7540872eabae5e3884607490154d52e67f5b5636
BrendanSD3 <brendan.wall03@gmail.com>
2020-11-22 21:04:44 +0000

    commiting,  added Blog Crud and tests test causing issues

--- Env/flaskr/auth.py
+++ Env/flaskr/auth.py
@@ -39,7 +39,7 @@ def register():
     return render_template('auth/register.html')
 
 
-    @bp.route('/login', methods=('GET', 'POST'))
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -64,7 +64,7 @@ def login():
 
     return render_template('auth/login.html')
 
-    @bp.before_app_request
+@bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
 
